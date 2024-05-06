58a3c23d169cdd849f79601e057d77cd49f935a5
sawa888 <oblige.tokyo@docomo.ne.jp>
2024-05-06 15:42:27 +0900

    コントローラーの before_action 設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   # ログインしていないユーザーはログインページへ遷移させる。
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   # ※N-1問題解決。一覧降順表示
   def index
@@ -21,18 +22,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     return if @item.user_id == current_user.id
 
     redirect_to root_path
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -46,4 +44,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :product_name, :present, :category_id, :condition_id, :delivery_charge_id,
                                  :shipping_address_id, :delivery_time_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
