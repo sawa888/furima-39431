7ecba1bb1c08f4f09b312299cef8ce7b01c60573
sawa888 <oblige.tokyo@docomo.ne.jp>
2024-05-13 17:32:56 +0900

    orderモデル、delivery_addressモデル作成
    orderモデル、delivery_addressモデル作成

--- app/models/order_delivery_address.rb
+++ app/models/order_delivery_address.rb
@@ -1,9 +1,12 @@
 class OrderDeliveryAddress
 
-  include ActivModel::module 
+  # include を使用し、N+1問題解消
+  include ActivModel::Model 
+  # attr_accessor を使用し、保存したいカラム名を属性値として扱えるようにする
   attr_accessor :user_id, :item_id, :token, :postal_code, :shipping_address_id, :city, :street_address, :building_name, :phone_number, :order_id 
   
-  end
+  # ordersテーブルとdelivery_addressesテーブルのバリデーションを記述(移動)
+  
   
 end
 
