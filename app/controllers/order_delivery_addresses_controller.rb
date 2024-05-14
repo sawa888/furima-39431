2b409dea1f73537559f11f9542bc6cb18414c35c
sawa888 <oblige.tokyo@docomo.ne.jp>
2024-05-14 18:46:56 +0900

    indexアクションの記述
    order_delivery_addressのindexアクションの記述。
    一部、createアクションの記述

--- app/controllers/order_delivery_addresses_controller.rb
+++ app/controllers/order_delivery_addresses_controller.rb
@@ -1,2 +1,29 @@
 class OrderDeliveryAddressesController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :set_order_delivery_address, only: [:index, :create]
+
+ def index
+  @order_delivery_address = OrderDeliveryAddress.new
+ end
+
+ def create
+  @order_delivery_address = OrderDeliveryAddress.new(order_delivery_address_params)
+end
+
+
+private 
+
+  def order_delivery_address_params
+    # この時点では、まだ商品を購入していない為(購入画面は確定ではない)、order_id は不要
+    params.require(:order_delivery_address).permit(:postal_code, :shipping_address_id, :city, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_order_delivery_address
+    @order = Item.find(params[:item_id])
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :items do
     resources :order_delivery_address, only: [:index, :create]
+  end
 
 end
 
