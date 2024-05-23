54c304941ab8fd36de663637a082ca94d26f62be
sawa888 <oblige.tokyo@docomo.ne.jp>
2024-05-23 20:04:17 +0900

    本番環境における修正
    出品に失敗した際、「販売手数料 (10%)と販売利益」が計算されない現象の修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,16 +1,5 @@
-// window.addEventListener('turbo:load', () => {
-//   const priceInput = document.getElementById("item-price");
-//   priceInput.addEventListener("input", () => {
-//     const inputValue = priceInput.value;
-//     const addTaxDom = document.getElementById("add-tax-price");
-//     const addTaxValue = Math.floor(inputValue) * 0.1; 
-//     addTaxDom.innerHTML = addTaxValue;
-//     const addProfitDom = document.getElementById("profit");
-//     addProfitDom.innerHTML = Math.floor(inputValue - addTaxValue);
-//   })
-// });
 
-window.addEventListener('turbo:load', () => {
+const price = () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = parseFloat(priceInput.value); // 入力値を数値型に変換
@@ -20,8 +9,10 @@ window.addEventListener('turbo:load', () => {
     const addProfitDom = document.getElementById("profit");
     addProfitDom.innerHTML = Math.floor(inputValue - addTaxValue); // 小数点以下を切り捨てた利益額
   })
-});
+};
 
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
 
 
 
